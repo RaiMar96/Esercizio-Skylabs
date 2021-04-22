c27388f4ec5735967c090528bcbd6cf00116f525
RaiMar96 <raitimario.mr@gmail.com>
2021-04-22 13:15:23 +0200

    Commit: Implementate le chiamate REST nel controller

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Esercizio-Skylabs
 Servizio REST con ASP.NET Core
+
+- **POST**  localhost:*{portNumber}*/api/TestWebItems/product
+
+- **GET**   localhost:*{portNumber}*/api/TestWebItems/product/*{ProductNAme}*
+
+Il body per la POST deve avere la struttura mostrata dalù json di esempio
+
+```json
+{
+ "nome" : "Notebook",
+ "prezzo" : 20.00
+}
+```
--- TestWebApi/TestWebApi/Controllers/TestWebItemsController.cs
+++ TestWebApi/TestWebApi/Controllers/TestWebItemsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -18,6 +19,49 @@ namespace TestWebApi.Controllers
         public TestWebItemsController(TestContext context)
         {
             _context = context;
+        }
+
+        /// <summary>
+        /// Metodo cha implementa la chiamta post che permette l'inserimento nel DB dell'oggetto passato nel body della richiesta
+        /// </summary>
+        /// <param name="testWebItem">Oggetto da inserire nel db composto dalla coppia nome e prezzo</param>
+        /// <returns>Oggetto inserito nel Bd in caso di successo, O messaggio di errore in caso di insuccesso</returns>
+        [HttpPost("product")]
+        public async Task<IActionResult> PostItem(TestWebItem testWebItem)
+        {
+            try { 
+                _context.Add(testWebItem);
+                var testWebItemRes = await _context.SaveChangesAsync();
+                return Ok("Oggetto Creato");
+            } 
+            catch(Exception)
+            {
+                return BadRequest("Errore Durante L'inserimento");
+            }
+        }
+
+        /// <summary>
+        /// Metodo che implementa la Get sugli oggetti del DB ricercando sul nome passato tramite url
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>Ritorna uno/nessuno/più elementi a seconda del risultato della query, se nessuno ritorna notFound</returns>
+        [HttpGet("product/{name}")]
+        public async Task<IActionResult> GetItemByName(string name)
+        {
+            if (name == null)
+            {
+                return NotFound();
+            }
+
+            var testWebItem = await _context.TestItems
+                .Where(m => Regex.IsMatch(m.Nome, name, RegexOptions.IgnoreCase))
+                .ToListAsync();
+            if (testWebItem == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(testWebItem);
         }
     }
 }
